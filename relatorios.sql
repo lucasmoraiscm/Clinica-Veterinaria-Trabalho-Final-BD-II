ec734be8af14a8e41cb63ad58935a3bf9d332b69
natyyHy <natielegrazielly5@gmail.com>
2025-06-21 16:45:31 -0300

    :sparkles:feat: adicionando as principais ideias de relatorios

---
+++ relatorios.sql
@@ -0,0 +1,12 @@
+
+--- RELATORIOS PARA A GESTÃO DA CLINICA ---
+
+
+
+--- (1) CONTROLE BASICO DE ESTOQUE DE FARMACOS (Baixo Estoque / Próximo da Validade)
+
+
+--- (2) RELATORIO DE RECEITA DETALHADA POR SERVICO/ESPECIALIDADE (Identificar os serviços mais lucrativos)
+
+
+--- (3) RELATORIO DE DIAGNOSTICOS MAIS COMUNS (Doenças são mais frequentes)
